d83312d4f1c0f3745fc5dfb6865f8a3f30dd4d27
Juan Dominguez <juan.dominguez.bdm@gmail.com>
2019-02-12 19:14:13 -0800

    Initial Commit

---
+++ JuanFlix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JuanFlix
+//
+//  Created by Juan Dominguez on 2/12/19.
+//  Copyright © 2019 Juan Dominguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
